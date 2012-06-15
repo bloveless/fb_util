7da2410419b6ce980e9cd8850f57c18413672ddd
Brennon Loveless <brennon@fritzandandre.com>
2012-06-15 17:11:18 -0600

    Updated the readme to have some good information as well as adding comments to the api class

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-facebook_graph_api
-==================
+# Facebook Graph API #
 
-Oh my simple facebook graph api
+## TODO ##
+1. Should have a way to get an access token since the entirety of the api is based off already having an api key. This will come.
+
+## Usage example ##
+
+To get the users feed
+
+**facebook_graph_api** = FacebookGraphAPI.new(<access token>)  
+facebook_graph_api.get_feed
+
+## Caching ##
+
+Most of the methods are cache so that each time a class is called it will store the results of any method call. This way if you want to call a message more than one time it will not query from facebook again, but rather return the cached version. The get_fql method is not cache since you could potentially run two different fql queries in the same instantiation of a class.
+
+## Methods ##
+_All methods are assuming the use of the above initilization code including a valid access token._  
+
+Get the basic account information from facebook  
+**facebook_graph_api**_.get_account_info_
+
+Get the users feed  
+**facebook_graph_api**_.get_feed_
+
+Post a message to the users wall  
+*  message: text of the message to be posted. Links will not be converted to click able links. Use post_link to post a clickable link (including video)_  
+**facebook_graph_api**_.post_status(message)_
+
+Post a picture to the users wall  
+*  picture_path: the actual file path to the image (no urls)  
+*  message: the message to be attached to the image if any. Can be null or empty.  
+*  post_to_feed: true (by default) if you want the picture to be posted to the users wall, false if you want it hidden. **This isn't 100% tested**  
+**facebook_graph_api**_.post_picture(picture_path, message, post_to_feed=true)_
+
+Set an already existing image to be the users cover image  
+*  picture_id: the facebook id of the image to use as the users cover image. **This currently doesn't allow for an offset, but this will be available in the next version**  
+**facebook_graph_api**_.set_as_cover_image(picture_id)_
+
+Post a clickable link to a users feed (works for youtube videos as well)  
+*  link: the link (beginning with http:// or https://) that will be displayed in the users feed  
+*  message: the message to be posted with the link on the feed page  
+**facebook_graph_api**_.post_link(link, message)_
+
+Post a reply to a status that is already existing on facebook
+*  status_id: the facebook id of the status to reply to  
+*  message: the message to use in the reply  
+**facebook_graph_api**_.reply_to_status(status_id, message)
+
+Delete a status  
+*  status_id: the facebook id of the status to delete  
+**facebook_graph_api**_.delete_status(status_id)
+
+Get statuses from facebook page/account  
+**facebook_graph_api**_.get_statuses_
+
+Get the insights of a page (**this doesn't work for profiles**)  
+**facebook_graph_api**_.get_insights_
+
+Get any fql request  
+*  fql: the facebook query requested  
+**facebook_graph_api**_.get_fql(fql)_
+
+Get pages associated with the account  
+**facebook_graph_api**_.get_pages_
--- lib/facebook_graph_api.rb
+++ lib/facebook_graph_api.rb
@@ -14,10 +14,12 @@ class FacebookGraphAPI
     end
   end
 
+  # Get the basic account information from facebook  
   def get_account_info
     @account_info ||= get_request('/me')
   end
 
+  # Get the users feed
   def get_feed
     @feed ||= get_request('/me/feed')
     # I have no idea why sometimes it uses the data index and sometimes it doesn't....
@@ -28,10 +30,16 @@ class FacebookGraphAPI
     end
   end
 
+  # Post a message to the users wall
+  # message: text of the message to be posted. Links will not be converted to click able links. Use post_link to post a clickable link (including video)
   def post_status(message)
     post_request('/me/feed', {message: message})
   end
 
+  # Post a picture to the users wall
+  # picture_path: the actual file path to the image (no urls)
+  # message: the message to be attached to the image if any. Can be null or empty.
+  # post_to_feed: true (by default) if you want the picture to be posted to the users wall, false if you want it hidden. This isn't 100% tested.
   def post_picture(picture_path, message, post_to_feed=true)
     File.open(picture_path.to_s.gsub('%20', '\ ').gsub('(', '\(').gsub(')', '\)'), 'rb') do |binary_image|
       if post_to_feed
@@ -42,26 +50,38 @@ class FacebookGraphAPI
     end
   end
 
+  # Set an already existing image to be the users cover image  
+  # picture_id: the facebook id of the image to use as the users cover image. This currently doesn't allow for an offset, but this will be available in the next version.
   def set_as_cover_image(picture_id)
     post_request('/me', {cover: picture_id, no_feed_story: 'true'})
   end
 
+  # Post a clickable link to a users feed (works for youtube videos as well)  
+  # link: the link (beginning with http:// or https://) that will be displayed in the users feed  
+  # message: the message to be posted with the link on the feed page  
   def post_link(link, message)
     post_request('/me/feed', {link: link, message: message})
   end
 
+  # Post a reply to a status that is already existing on facebook
+  # status_id: the facebook id of the status to reply to  
+  # message: the message to use in the reply  
   def reply_to_status(status_id, message)
     post_request('/' + status_id + '/comments', {message: message})
   end
 
+  # Delete a status  
+  # status_id: the facebook id of the status to delete  
   def delete_status(status_id)
     delete_request('/' + status_id)
   end
 
+  # Get statuses from facebook page/account  
   def get_statuses
     @statuses ||= get_request('/me/statuses')
   end
 
+  # Get the insights of a page (**this doesn't work for profiles**)  
   def get_insights
     @insights ||= get_request('/me/insights')
     begin
@@ -71,29 +91,40 @@ class FacebookGraphAPI
     end
   end
 
-  # Fql cannot be cached since it might be used more than once to gather different data
-  # without creating a new facebook api class
+  # Get any fql request
+  # Fql cannot be cached since it might be used more than once to gather different data without creating a new facebook api class
   def get_fql(fql)
     get_request('/fql?q=' + CGI.escape(fql))['data']
   end
 
+  # Get pages associated with the account
   def get_pages
     @page ||= get_request('/me/accounts')['data']
   end
 
   private
+  # Execute a get request against the facebook endpoint requested
+  # end_point: anything in the url after the graph.facebook.com i.e. /me/accounts
   def get_request(end_point)
     execute_request(end_point, 'get')
   end
 
+  # Send data to the facebook endpoint requested
+  # end_point: anything in the url after the graph.facebook.com i.e. /me/accounts
+  # parameters: the information to be sent to facebook in a hash
   def post_request(end_point, parameters)
     execute_request(end_point, 'post', parameters)
   end
 
+  # Delete end point from facebook
   def delete_request(end_point)
     execute_request(end_point, 'delete')
   end
 
+  # The actual request send to facebook
+  # end_point: anything in the url after the graph.facebook.com i.e. /me/accounts
+  # method: the HTTP method used to send or receive information from facebook
+  # parameters: the information to be send to facebook in a hash
   def execute_request(end_point, method, parameters = {})
     agent = Mechanize.new
     begin
